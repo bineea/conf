ebd3931e6420f318524bd707b4b97d60a3f96b83
bineea <33547731+bineea@users.noreply.github.com>
2020-03-20 00:23:52 +0800

    对称加密配置信息

--- src/main/java/my/blog/conf/ConfApplication.java
+++ src/main/java/my/blog/conf/ConfApplication.java
@@ -3,10 +3,10 @@ package my.blog.conf;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 
 //因为Spring Cloud Config 服务是Spring Boot应用程序，因此需要用@SpringBootApplication进行标记
 @SpringBootApplication
-
 //@EnableConfigServer使服务成为Spring Cloud Config服务
 @EnableConfigServer
 public class ConfApplication {
